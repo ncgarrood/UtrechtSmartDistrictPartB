94c3780561273fb821a499583ed79240cc2a9167
WillemvanRossum <willem.van.rossum@ziggo.nl>
2021-10-21 17:00:08 +0200

    Legends not in front of graph

--- AssB_Group4_costs.py
+++ AssB_Group4_costs.py
@@ -115,7 +115,7 @@ def get_plots_costs(season1, season2):
         axs[0].plot(season1.SoC, label = 'SoC')
         axs[0].plot(season1['Residential load [kW]'], label = 'Pdem')
 
-        axs[0].legend(loc='upper right')
+        axs[0].legend(loc='upper right', bbox_to_anchor=(1.2,1))
         axs[0].set(ylabel='Power (kW)', title='Summer')
 
         
@@ -125,7 +125,7 @@ def get_plots_costs(season1, season2):
         axs[1].plot(season2.SoC, label = 'SoC')
         axs[1].plot(season2['Residential load [kW]'], label = 'Pdem')
 
-        axs[1].legend(loc='upper right')
+        axs[1].legend(loc='upper right', bbox_to_anchor=(1.2,1))
         axs[1].set(ylabel='Power (kW)',xlabel='Time (h/4)', title='Winter')
          
 get_plots_costs(summer_out_costmin, winter_out_costmin)
